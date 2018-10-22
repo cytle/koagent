b02f333e95ad569e2d7dbbc4430ca3c3258cf130
炒饭 <chaofan@2dfire.com>
2018-10-22 17:14:54 +0800

    feat: koagent-certificate just manage certificate

--- packages/koagent-certificate/src/index.ts
+++ packages/koagent-certificate/src/index.ts
@@ -1,31 +1,27 @@
-import fs from 'fs-extra';
-import Koa from 'koa';
-import KoaRouter from 'koa-router';
 import _ from 'lodash';
-import { ICertificateOptions, ICertificateService } from './interfaces';
+import { ICertificateOptions } from './interfaces';
 import { CertificateService } from './CertificateService';
 import { CertificateStorage } from './CertificateStorage';
 
-declare module 'koa' {
-  interface BaseContext {
-    certificate: ICertificateService;
-  }
-}
+// declare module 'koa' {
+//   interface BaseContext {
+//     certificate: ICertificateService;
+//   }
+// }
 
-export function createCertificateService(options: ICertificateOptions) {
-  fs.ensureDir(options.storagePath);
+export default function createCertificateService(options: ICertificateOptions) {
   const certStorage = new CertificateStorage(options, {});
   return new CertificateService(options.rootKey, certStorage);
 }
-export default (app: Koa, router: KoaRouter, options: ICertificateOptions) => {
-  const certService = createCertificateService(options);
-  app.context.certificate = certService;
+// export default (app: Koa, router: KoaRouter, options: ICertificateOptions) => {
+//   const certService = createCertificateService(options);
+//   app.context.certificate = certService;
 
-  const selfRouter = new KoaRouter();
-  selfRouter.get('/rootCA', async (ctx) => {
-    ctx.set('Content-disposition', 'attachment;filename=koagentCA.crt');
-    ctx.body = (await certService.getRoot()).cert;
-  });
+//   const selfRouter = new KoaRouter();
+//   selfRouter.get('/rootCA', async (ctx) => {
+//     ctx.set('Content-disposition', 'attachment;filename=koagentCA.crt');
+//     ctx.body = (await certService.getRoot()).cert;
+//   });
 
-  router.use('/certificate', selfRouter.routes(), selfRouter.allowedMethods());
-};
+//   router.use('/certificate', selfRouter.routes(), selfRouter.allowedMethods());
+// };
