31bee4f77631e045be8cccf40c6d33dc4704e4a4
炒饭 <chaofan@2dfire.com>
2018-10-24 13:25:34 +0800

    feat(koagent-certificate): add middleware

--- packages/koagent-certificate/src/index.ts
+++ packages/koagent-certificate/src/index.ts
@@ -1,3 +1,4 @@
+import KoaRouter from 'koa-router';
 import _ from 'lodash';
 export * from './interfaces';
 export * from './CertificateService';
@@ -6,25 +7,19 @@ import { ICertificateOptions } from './interfaces';
 import { CertificateService } from './CertificateService';
 import { CertificateStorage } from './CertificateStorage';
 
-// declare module 'koa' {
-//   interface BaseContext {
-//     certificate: ICertificateService;
-//   }
-// }
+export default (
+  koangetCtx,
+) => {
+  const selfRouter = new KoaRouter();
+  selfRouter.get('/rootCA', async (ctx) => {
+    ctx.set('Content-disposition', 'attachment;filename=koagentCA.crt');
+    ctx.body = (await koangetCtx.certService.getRoot()).cert;
+  });
+  koangetCtx.router.use('/certificate', selfRouter.routes(), selfRouter.allowedMethods());
+  return (_ctx, next) => next();
+};
 
-export default function createCertificateService(options: ICertificateOptions) {
+export function createCertificateService(options: ICertificateOptions) {
   const certStorage = new CertificateStorage(options, {});
   return new CertificateService(options.rootKey, certStorage);
 }
-// export default (app: Koa, router: KoaRouter, options: ICertificateOptions) => {
-//   const certService = createCertificateService(options);
-//   app.context.certificate = certService;
-
-//   const selfRouter = new KoaRouter();
-//   selfRouter.get('/rootCA', async (ctx) => {
-//     ctx.set('Content-disposition', 'attachment;filename=koagentCA.crt');
-//     ctx.body = (await certService.getRoot()).cert;
-//   });
-
-//   router.use('/certificate', selfRouter.routes(), selfRouter.allowedMethods());
-// };
