394918d0bc93a5415833c15a387679b5431f6c2a
炒饭 <chaofan@2dfire.com>
2018-10-25 16:20:10 +0800

    chore: update script

--- packages/koagent/src/scripts/postinstall.ts
+++ packages/koagent/src/scripts/postinstall.ts
@@ -1,6 +1,6 @@
 import fs from 'fs-extra';
 import path from 'path';
-import config from '../src/config';
+import config from '../config';
 
 Promise.resolve().then(async () => {
   await fs.remove(config.certifacateStoragePath);
