f782192a75b1e25e297c3f584b58bd620ead8a4b
炒饭 <chaofan@2dfire.com>
2018-10-31 16:52:02 +0800

    chore: remove webpack

--- packages/koagent-client/src/pages/Monitor.vue
+++ packages/koagent-client/src/pages/Monitor.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Monitor',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'Monitor',
     };
   },
 };
--- packages/koagent-client/src/pages/ReverseProxy/index.vue
+++ packages/koagent-client/src/pages/ReverseProxy/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
   </div>
 </template>
 
@@ -13,7 +12,7 @@ export default {
   name: 'ReverseProxy',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'ReverseProxy',
     };
   },
   beforeCreate() {
